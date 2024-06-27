c550198ede18acff5f414786b261a24e969d4558
Ann <sokolikganna@gmail.com>
2024-06-26 22:05:50 -0400

    Add changes have been made to all sections before login

--- frontend/src/pages/SitePages/ContactPage.js
+++ frontend/src/pages/SitePages/ContactPage.js
@@ -3,81 +3,42 @@ import Layout from "../../components/Layout/LayoutSite";
 
 const ContactPage = () => {
   return (
-    <Layout>
-      <div className="contact">
-        <header className="contact-header">
+
+      <Layout>
+      <div className="contact-page">
+        <div className="contact-header">
           <h1>Contact us</h1>
-          <p>
-            Email us: <a href="mailto:greenduo@gmail.com">greenduo@gmail.com</a>
-          </p>
-        </header>
+          <p className="email-us">Email us: <a href="mailto:greenduo@gmail.com">greenduo@gmail.com</a></p>
+        </div>
         <div className="feedback-section">
-          <h2>Give Feedback</h2>
-          <p>
-            We’d love to hear from you. How are you feeling about Food Sage?
-          </p>
+          <h2 className="form-name">Give Feedback</h2>
+          <p>We’d love to hear from you. How are you feeling about Food Sage?</p>
           <form className="feedback-form">
-            <div>
-              <input
-                type="checkbox"
-                id="cant-find"
-                name="feedback"
-                value="cant-find"
-              />
-              <label htmlFor="cant-find"> I can’t find something</label>
+            <label>
+              <input type="checkbox" name="feedback" value="cant-find" />
+              I can’t find something
+            </label>
+            <label>
+              <input type="checkbox" name="feedback" value="cant-figure-out" />
+              I can’t figure out how to do something
+            </label>
+            <label>
+              <input type="checkbox" name="feedback" value="figured-out" />
+              I figured how to do something, but it was too hard to do.
+            </label>
+            <label>
+              <input type="checkbox" name="feedback" value="love-it" />
+              I love it!
+            </label>
+            <label>
+              <input type="checkbox" name="feedback" value="something-else" />
+              Something else.
+            </label>
+            <div className="additional-feedback">
+              <label>Anything else you’d like to share?</label>
+              <textarea placeholder="I have feedback on..."></textarea>
             </div>
-            <div>
-              <input
-                type="checkbox"
-                id="cant-figure-out"
-                name="feedback"
-                value="cant-figure-out"
-              />
-              <label htmlFor="cant-figure-out">
-                {" "}
-                I can’t figure out how to do something
-              </label>
-            </div>
-            <div>
-              <input
-                type="checkbox"
-                id="figured-out"
-                name="feedback"
-                value="figured-out"
-              />
-              <label htmlFor="figured-out">
-                {" "}
-                I figured out how to do something, but it was too hard to do
-              </label>
-            </div>
-            <div>
-              <input
-                type="checkbox"
-                id="love-it"
-                name="feedback"
-                value="love-it"
-              />
-              <label htmlFor="love-it"> I love it!</label>
-            </div>
-            <div>
-              <input
-                type="checkbox"
-                id="something-else"
-                name="feedback"
-                value="something-else"
-              />
-              <label htmlFor="something-else"> Something else</label>
-            </div>
-            <div className="feedback-input">
-              <label htmlFor="additional-feedback">
-                Anything else you’d like to share?
-              </label>
-              <textarea
-                id="additional-feedback"
-                name="additional-feedback"
-                placeholder="I have feedback on..."
-              ></textarea>
-            </div>
+            <button type="submit" className="submit-button">Submit</button>
           </form>
         </div>
       </div>
