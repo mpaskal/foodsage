0bac4a018c92d96d562a10459bc1e27e4d71386c
Marina Paskal <paskalm@rogers.com>
2024-08-03 22:19:06 -0400

    Update client connection

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,12 +1,9 @@
 import axios from "axios";
 
 const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL ||
-  (process.env.NODE_ENV === "production"
-    ? "/api"
-    : "http://localhost:5000/api");
+  process.env.REACT_APP_API_BASE_URL || "http://localhost:5000/api";
 
-console.log("API Base URL:", API_BASE_URL); // Add this for debugging
+console.log("API Base URL:", API_BASE_URL); // Keep this for debugging
 
 const api = axios.create({
   baseURL: API_BASE_URL,
